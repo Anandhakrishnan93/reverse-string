2e86ba4f9258c1ce4540c9a3c3413bb061303e46
Anandhakrishnan93 <125742093+Anandhakrishnan93@users.noreply.github.com>
2023-11-06 17:43:34 -0800

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  StringReverse
+//
+//  Created by Anandhakrishnan TR
+//
+
+import Foundation
+
+print(reverse("Hello"))
+
+func reverse( _ str:String) ->  String{
+    let str1 = Array(str)
+    if str1.count == 1{
+        return "\(str1[0])"
+    }
+    let newstr = String(str1.dropFirst())
+    let x = reverse(newstr)
+    return x + String(str1[0])
+}
